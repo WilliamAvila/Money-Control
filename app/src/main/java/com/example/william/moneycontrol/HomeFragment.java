02833e45e1f68decdb18714383cce377d15ebd26
William Avila <william@Williams-Mac-mini.local>
2015-01-31 20:18:57 -0600

    Adding Left Menu

---
+++ app/src/main/java/com/example/william/moneycontrol/HomeFragment.java
@@ -0,0 +1,24 @@
+package com.example.william.moneycontrol;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by william on 1/31/15.
+ */
+public class HomeFragment extends Fragment {
+
+    public HomeFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/william/moneycontrol/PeopleFragment.java
@@ -0,0 +1,51 @@
+package com.example.william.moneycontrol;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.william.moneycontrol.R;
+
+/**
+ * Created by william on 1/31/15.
+ */
+public class PeopleFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_people, container, false);
+
+
+        Button btnNextScreen = (Button) rootView.findViewById((R.id.btnNextScreen));
+
+
+
+        btnNextScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextScreen = new Intent(getActivity().getApplicationContext(),SecondScreenActivity.class);
+
+                startActivity(nextScreen);
+            }
+        });
+
+
+
+
+
+
+        return rootView;
+
+
+    }
+
+
+
+}
