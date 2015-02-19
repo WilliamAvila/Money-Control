42026200bcc1484a80737fea4099234cc32729fc
Jimmy Ramos <jimmy.ramos@unitec.edu>
2015-02-19 14:29:15 -0600

    Informes

--- app/src/main/java/com/example/william/moneycontrol/InformesFragment.java
+++ app/src/main/java/com/example/william/moneycontrol/InformesFragment.java
@@ -1,11 +1,19 @@
 package com.example.william.moneycontrol;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import static android.R.*;
 
 
 /**
@@ -14,17 +22,13 @@ import android.view.ViewGroup;
 public class InformesFragment extends Fragment {
 
 
-    public InformesFragment() {
-        // Required empty public constructor
-    }
-
-
+    private View rootView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_informes, container, false);
+
+        rootView = inflater.inflate(R.layout.fragment_informes, container, false);
+
+        return rootView;
     }
-
-
 }
